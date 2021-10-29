b4804463deeaefb6117aa46de5cc35a2fc3f5b11
johanneshermans <johannes.hermans@student.howest.be>
2021-10-30 00:53:42 +0200

    Updated concept page

--- src/pages/concept.js
+++ src/pages/concept.js
@@ -1,17 +1,37 @@
 import React from "react";
 const Concept = () => {
     return (
-        <div className="short">
-            <section className="post__container">
-                <h2>How can an AR experience gain more
-                    experience and interaction out of
-                    a TV moment in video clips?</h2>
+        <article className="short">
+            <h2>Explore the video clips of tomorrow, today</h2>
 
-                <p className="alone">test</p>
+            <section className="post__container">
+                <h3>How can an AR experience gain a better
+                    experience and interaction out of
+                    a TV moment in video clips?</h3>
+                <p>The experience will mainly entertain people while watching a music video. Now music videos are 2D but how fun would it be when you watch a music video that a real stage or visuals come to life in your living room.</p>
+
+
+            </section>
+
+            <section className="post__container">
+                <h3>Apple Music</h3>
+                <p>I chose Apple music as a client because it fits within the Apple ecosystem that I will largely be working in and for. This could just as easily have been Deezer, Spotify, Youtube(music) or Tidal. Platforms that make music go viral. What reinforces my choice of Apple Music is the fact that Apple is working on AR glasses, I had included that idea in my concept as well because I am convinced that when they will launch the glasses this will be a feature.</p>
+
+                <h4>Resources</h4>
+                <a href="https://www.inventiva.co.in/trends/kritisrivastava/apple_ar_glasses/">APPLE AR GLASSES TO MAKE AN AMAZING GRAND DEBUT IN 2022?</a>
+                <a href="https://www.phonearena.com/news/apple-augmented-reality-glasses-the-future-of-smartphones_id135935">AR is the future of smartphones, starting with Apple's AR glasses</a>
+                <a href="https://global.techradar.com/nl-be/news/apple-glasses-projecteert-mogelijk-een-scherm-recht-op-je-ogen">Apple Glasses projecteert mogelijk een scherm recht op je ogen</a>
+                <a href="https://news.sky.com/story/facebook-and-apple-smart-glasses-will-soon-be-upon-us-but-are-they-just-another-fad-12396720">Facebook and Apple smart glasses will soon be upon us - but are they just another fad?</a>
+            </section>
+
+            <section className="post__container">
+                <h3>Deliverables</h3>
+                <p>Will be soon availible</p>
+
             </section>
 
 
-        </div>
+        </article>
     )
 }
 
